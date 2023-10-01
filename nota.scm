29aa7f2f80df6ea4b0ba54a29473c1b1e221192a
long_season <teotsouk@gmail.com>
2023-10-01 07:47:56 +0300

    basic repl, file settings, cleanup

--- nota.scm
+++ nota.scm
@@ -1,36 +1,66 @@
 ; TODO:
 ; - some sort of repl for constant challenges
-; - remove prints where possible
 ; - add range for frets >12
 ; - smarter way to populate notes to frets in *-string-{l,s} lists (macro)
 ; - make it possible to tweak preferences more intuitively
-; - add the rest of the strings
-
-(import srfi-216)
+; - fix compiled version (problem caused by list-ref or similar probably)
+; - read notation?/fret? from settings.txt + fix newline + avoid append if existing
+;
+(import srfi-216) ; implements random
+(import (chicken string)) ; implements string-chop, ->string
+(import (chicken file posix)) ; file i/o
 (load "strings.scm")
 
 (define letter-notation? (lambda () (print "Use Letter (C, C#) or Solfege (Do, Do#) notation? (l)etter/(s)olfege")
   (let ((notation-choice (read) ))
+    (define settingf (file-open "settings.txt" (+ open/append open/wronly open/creat)))
     (if (equal? notation-choice 'l)
-    #t
-    #f))))
+        (file-write settingf "notation l ")
+        (file-write settingf "notation s "))
+    (file-close settingf))))
 
 (define fret? (lambda () (print "Guess the frets of each note, or a note from each fret? (f)ret/(n)ote")
-  (let ((notation-choice (read) ))
-    (if (equal? notation-choice 'l)
-    #t
-    #f))))
+  (let ((fret-choice (read) ))
+    (define settingf (file-open "settings.txt" (+ open/append open/wronly open/creat)))
+    (if (equal? fret-choice 'f)
+        (file-write settingf "fret f")
+        (file-write settingf "fret n"))
+    (file-close settingf))))
 
 (define get-fret (lambda ()
                    (if (letter-notation?)
-                       (print (car (list-ref all-l
-                                             (random (length all-l)))))
-                       (print (car (list-ref e-string-s
-                                    (random (length e-string-s))))))))
+                       (car (list-ref all-l
+                                             (random (length all-l))))
+                       (car (list-ref all-s
+                                    (random (length all-s)))))))
+
 (define get-note (lambda ()
                    (if (letter-notation?)
-                    (print (cdr (list-ref e-string-l
-                                  (random (length e-string-l)))))
-                    (print (cdr (list-ref e-string-s
-                                          (random (length e-string-s))))
-                     ))))
+                    (cdr (list-ref all-l
+                                  (random (length all-l))))
+                    (cdr (list-ref all-s
+                                          (random (length all-s)))))))
+
+; Compart.?
+;(define check-guess (lambda ()
+;                        ; Currently invalidates get-note/get-fret, implement above
+;                        (let (( note `(,(list-ref all-l (random (length all-l))),)' ))
+;                          (print "Find " (cadar note) " on the " (car (string-chop (->string (caar note)) 1)) " string. \
+;                          (e.g. 'G2'): ")
+;                          (let ((guess (read)))
+;                            (if (member guess (car note))
+;                                (print "Correct!")
+;                                (print "Wrong!"))))))
+
+(define repl
+  (lambda ()
+    (let (( note `(,(list-ref all-l (random (length all-l))),)' ))
+      (print "Find " (cadar note) " on the " (car (string-chop (->string (caar note)) 1)) " string. \
+             (e.g. 'G2'): ")
+      (let ((guess (read)))
+        (if (eq? (car guess) 'quit)
+            (exit)
+        (if (member guess (car note))
+            (print "Correct!")
+            (print "Wrong!"))))
+    (repl))))
--- strings.scm
+++ strings.scm
@@ -14,5 +14,29 @@
                      (La5 Re) (La6 Re#) (La7 Mi) (La8 Fa)
                      (La9 Fa#) (La10 Sol) (La11 Sol#)))
 
-(define all-l (append e-string-l a-string-l))
-(define all-s (append e-string-s a-string-s))
+(define d-string-l '((D0 D) (D1 D#) (D2 E) (D3 F) (D4 F#)
+                     (D5 G) (D6 G#) (D7 A) (D8 A#)
+                     (D9 B) (D10 C) (D11 C#)))
+
+(define d-string-s '((Re0 Re) (Re1 Re#) (Re2 Mi) (Re3 Fa) (Re4 Fa#)
+                     (Re5 Sol) (Re6 Sol#) (Re7 La) (Re8 La#)
+                     (Re9 Si) (Re10 Do) (Re11 Do#)))
+
+(define g-string-l '((G0 G) (G1 G#) (G2 A) (G3 A#) (G4 B)
+                     (G5 C) (G6 C#) (G7 D) (G8 D#)
+                     (G9 E) (G10 F) (G11 F#)))
+
+(define g-string-s '((Sol0 Sol) (Sol1 Sol#) (Sol2 La) (Sol3 La#) (Sol4 Si)
+                     (Sol5 Do) (Sol6 Do#) (Sol7 Re) (Sol8 Re#)
+                     (Sol9 Mi) (Sol10 Fa) (Sol11 Fa#)))
+
+(define b-string-l '((B0 B) (B1 C) (B2 C#) (B3 D) (B4 D#)
+                     (B5 E) (B6 F) (B7 F#) (B8 G)
+                     (B9 G#) (B10 A) (B11 A#)))
+
+(define b-string-s '((Si0 Si) (Si1 Do) (Si2 Do#) (Si3 Re) (Si4 Re#)
+                     (Si5 Mi) (Si6 Fa) (Si7 Fa#) (Si8 Sol)
+                     (Si9 Sol#) (Si10 La) (Si11 La#)))
+
+(define all-l (append e-string-l a-string-l d-string-l g-string-l b-string-l))
+(define all-s (append e-string-s a-string-s d-string-s g-string-s b-string-s))
